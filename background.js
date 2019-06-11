c1355b923f4e634d4f114a1a05dddde7324fc6e9
tako <rina.m.takase@gmail.com>
2019-06-11 11:38:31 +0900

    Error handling

--- background.js
+++ background.js
@@ -6,7 +6,9 @@ Object.entries(document.getElementsByClassName('write-content')).forEach ((el) =
     Object.entries(document.querySelector('ul[role="listbox"].suggester-container').getElementsByTagName('li')).forEach((li) => {
       if (li[1].getElementsByTagName('img').length < 1) {
         li[1].setAttribute("style", "position: relative");
-        Object.entries(span = li[1].getElementsByTagName('span'))[0][1].setAttribute("style", "margin-left: 30px;");
+        if (li[1].getElementsByTagName('span')) {
+          Object.entries(li[1].getElementsByTagName('span'))[0][1].setAttribute("style", "margin-left: 30px;");
+        }
         const id = /suggester-\d+/.exec(li[1].getAttribute('id'))[0].substr(10);
         const imgTag = document.createElement('img');
         imgTag.src = `https://avatars3.githubusercontent.com/u/${id}`
