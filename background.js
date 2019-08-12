79e6eb18086bfece2c0a407073e8885a3eaa84d2
tako <rina.m.takase@gmail.com>
2019-08-12 20:22:25 +0900

    Refactor

--- background.js
+++ background.js
@@ -3,13 +3,13 @@ Object.entries(document.getElementsByClassName('write-content')).forEach ((el) =
     if (!document.querySelector('ul[role="listbox"].suggester-container')) {
       return;
     }
-    Object.entries(document.querySelector('ul[role="listbox"].suggester-container').getElementsByTagName('li')).forEach((li) => {
-      if (li[1].getElementsByTagName('img').length < 1) {
-        const id = /suggester-\d+/.exec(li[1].getAttribute('id'))[0].substr(10);
+    Array.from(document.querySelector('ul[role="listbox"].suggester-container').getElementsByTagName('li')).forEach((li) => {
+      if (li.getElementsByTagName('img').length < 1) {
+        const id = /suggester-\d+/.exec(li.getAttribute('id'))[0].substr(10);
         const imgTag = document.createElement('img');
         imgTag.src = `https://avatars3.githubusercontent.com/u/${id}`
         imgTag.style = 'position: absolute; width: 20px; height: 20px;'
-        li[1].insertBefore(imgTag, li[1].firstChild);
+        li.insertBefore(imgTag, li.firstChild);
       }
     })
   };
